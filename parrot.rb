c0a2d1cf1f22d794694ae9a28b6bd36f75414c2d
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-06 21:17:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
- def parrot (name = "Squawk!")
- puts "#{name}" 
+ def parrot (phrase = "Squawk!")
+ puts "#{phrase}" 
  name = "Squawk!"
  
  end
